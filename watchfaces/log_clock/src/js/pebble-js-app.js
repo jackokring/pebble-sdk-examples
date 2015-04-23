923a2aa8f14f46eea5c9d65285494beb3d874250
Simon Jackson (Netbook) <jackokring@gmail.com>
2015-04-23 21:31:59 +0100

    added in a template for a GPS js

---
+++ watchfaces/log_clock/src/js/pebble-js-app.js
@@ -0,0 +1,65 @@
+function fetchWeather(latitude, longitude) {
+  var req = new XMLHttpRequest();
+  req.open('GET', "http://api.openweathermap.org/data/2.5/weather?" +
+    "lat=" + latitude + "&lon=" + longitude + "&cnt=1", true);
+  req.onload = function(e) {
+    if (req.readyState == 4) {
+      if(req.status == 200) {
+        console.log(req.responseText);
+
+        var response = JSON.parse(req.responseText);
+        var temperature = Math.round(response.main.temp - 273.15);
+        var icon = iconFromWeatherId(response.weather[0].id);
+        var city = response.name;
+        console.log(temperature);
+        console.log(icon);
+        console.log(city);
+        Pebble.sendAppMessage({
+          "WEATHER_ICON_KEY":icon,
+          "WEATHER_TEMPERATURE_KEY":temperature + "\u00B0C",
+          "WEATHER_CITY_KEY":city}
+        );
+
+      } else {
+        console.log("Error");
+      }
+    }
+  }
+  req.send(null);
+}
+
+function locationSuccess(pos) {
+  var coordinates = pos.coords;
+  fetchWeather(coordinates.latitude, coordinates.longitude);
+}
+
+function locationError(err) {
+  console.warn('location error (' + err.code + '): ' + err.message);
+  Pebble.sendAppMessage({
+    "WEATHER_CITY_KEY":"Loc Unavailable",
+    "WEATHER_TEMPERATURE_KEY":"N/A"
+  });
+}
+
+var locationOptions = { "timeout": 15000, "maximumAge": 60000 }; 
+
+Pebble.addEventListener("ready", function(e) {
+  console.log("connect!" + e.ready);
+  locationWatcher = window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+  console.log(e.type);
+});
+
+Pebble.addEventListener("appmessage", function(e) {
+  window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+  console.log(e.type);
+  console.log(e.payload.temperature);
+  console.log("message!");
+});
+
+Pebble.addEventListener("webviewclosed", function(e) {
+  console.log("webview closed");
+  console.log(e.type);
+  console.log(e.response);
+});
+
+
