c55c74858f1364fac367543f74911e4475c947a6
Simon Jackson (Netbook) <jackokring@gmail.com>
2015-05-02 12:29:13 +0100

    Starts of an app

---
+++ watchapps/RoboMaze/build/c4che/_cache.py
@@ -0,0 +1,6 @@
+BINDIR = '/usr/local/bin'
+DEFINES = ['RELEASE']
+LIBDIR = '/usr/local/lib'
+PEBBLE_SDK_COMMON = '/home/jacko/pebble-dev/PebbleSDK/Pebble/common'
+PREFIX = '/usr/local'
+TARGET_PLATFORMS = [u'basalt', u'aplite']
