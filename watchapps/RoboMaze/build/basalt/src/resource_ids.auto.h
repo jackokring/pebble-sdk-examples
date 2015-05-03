9baa21c6b73e0abc5955852fede7963a709d8b84
Simon Jackson (Netbook) <jackokring@gmail.com>
2015-05-03 18:19:27 +0100

    made full screen area

--- watchapps/RoboMaze/build/basalt/src/resource_ids.auto.h
+++ watchapps/RoboMaze/build/basalt/src/resource_ids.auto.h
@@ -12,6 +12,7 @@ typedef enum {
   INVALID_RESOURCE = 0,
   RESOURCE_ID_INVALID = 0,
   DEFAULT_MENU_ICON = 0, // Friendly synonym for use in `PBL_APP_INFO()` calls
-  RESOURCE_ID_IMAGE_MENU_ICON_1, // 1
+  RESOURCE_ID_IMAGE_MENU_ICON, // 1
+  RESOURCE_ID_MAP, // 2
 
 } ResourceId;
