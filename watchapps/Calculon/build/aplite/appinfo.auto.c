d47db41f6c61a8a2393ffe1d6639d4e824d8ef35
Simon Jackson (Netbook) <jackokring@gmail.com>
2015-10-06 18:33:11 +0100

    Build test

---
+++ watchapps/Calculon/build/aplite/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 0, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Calculon",
+  .company = "K Ring Technologies",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = 0,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x71, 0x20, 0xF8, 0xE0, 0xEA, 0xC1, 0x4E, 0x41, 0xA2, 0xF3, 0x94, 0xF0, 0xE2, 0xC2, 0xC2, 0xA7 },
+  .virtual_size = 0xb6b6
+};
