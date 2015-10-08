dd777b4b0111f27d587f893499fff5b70f9e1b92
Simon Jackson (Netbook) <jackokring@gmail.com>
2015-10-08 18:25:43 +0100

    Obvious

--- watchapps/RoboMaze/src/basik.c
+++ watchapps/RoboMaze/src/basik.c
@@ -81,7 +81,7 @@ void click_value(ButtonId b, bool single) {
 	}
   } else {// edit the value on sreen
 	if(b == BUTTON_ID_UP) {
-		if(single) value = value * tenth;
+		if(single) value = value - one;
 		else value = value * ten;
 	} else {
 		if(single) value = value - one;
